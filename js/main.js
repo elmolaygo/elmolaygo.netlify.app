527e8be991f25929ffb85c08a160ef7b066f82d1
Alphi Laygo <elmolaygo.1@gmail.com>
2024-09-18 15:49:05 +0800

    navbar design finalized v1

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var toggle = document.getElementById("toggle")
-var drawer = document.getElementById("drawer")
-var logo = document.getElementById("logo")
-
-toggle.onclick = function() {
-  toggle.classList.toggle("toggle")
-  drawer.classList.toggle("toggle")
-  logo.classList.toggle("toggle")
-}
